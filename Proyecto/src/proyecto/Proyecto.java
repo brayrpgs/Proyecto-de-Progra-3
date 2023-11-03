541573eb47b1dd0ef9b13d63330e90fbe2e3ad42
kenda <kenda@Fallas>
2023-11-02 21:22:48 -0600

    Probando mensaje kendall estuvo aqui

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -16,6 +16,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("Kendall estuvo aqui");
     }
     
 }
