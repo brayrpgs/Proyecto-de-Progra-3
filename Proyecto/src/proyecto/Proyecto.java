c84a131550552a588a31c9915b72940f3b4a4e75
Ceasa <Ceasa@Lenovo-Ceasar>
2023-11-02 23:29:16 -0600

    modificando syso

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -15,7 +15,7 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola mundo 77");
         System.out.println("Rebecca novia de Kendall");
     }
     
