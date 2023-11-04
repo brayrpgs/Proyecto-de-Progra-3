0e2a183137dde876423177482fe380b2a21ab35c
brayrpgs <brayrpgs@brayrpgs-A320M-H>
2023-11-04 16:07:24 -0600

    actualizando agrego clientes

---
+++ Proyecto/src/domain/Customer.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author brayrpgs
+ */
+public class Customer {
+    private String id;
+    private String idCard;
+    private String name;
+    private String lastName;
+    private String phone;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String idCard, String name, String lastName, String phone) {
+        this.id = id;
+        this.idCard = idCard;
+        this.name = name;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", idCard=" + idCard + ", name=" + name + ", lastName=" + lastName + ", phone=" + phone + '}';
+    }
+    
+}
