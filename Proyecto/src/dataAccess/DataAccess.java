3ff48056783992d9c8edcb41a84338cf03038a6c
Ceasa <Ceasa@Lenovo-Ceasar>
2023-11-04 23:04:21 -0600

    Creacion de la lista empleados para los CRUD

--- Proyecto/src/dataAccess/DataAccess.java
+++ Proyecto/src/dataAccess/DataAccess.java
@@ -9,6 +9,8 @@ import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import domain.Employee;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,7 +18,12 @@ import domain.Employee;
  * @author Ceasar
  */
 public class DataAccess {
+    
     public boolean login(Employee employeeComeFromLogic){
+        /*
+        Metodo para validar el login en la pantalla principal,con este booleano 
+        llega a logica y en presentacion se pintan
+        */
         try{
             
             Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1/dbinventory", "root", "");
@@ -38,6 +45,36 @@ public class DataAccess {
             return false;
         }
     }
-    
+     public List<Employee> consultarTodosLosRegistrosEnBaseDeDatos(){
+        List<Employee>laListaDeRegistrosADevolver = new ArrayList<>();
+        try{
+            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1/dbinventory", "root", "");
+            PreparedStatement sentencia = (PreparedStatement) cn.prepareStatement("SELECT * FROM tbemployee");
+            
+            ResultSet rs = sentencia.executeQuery();
+                        
+            while(rs.next()) { //¿Existen registros?
+                //Seteo un empleado
+                Employee aEmployee = new Employee();    
+                aEmployee.setId(rs.getString("id"));
+                aEmployee.setIdCard(rs.getString("idCard"));
+                aEmployee.setName(rs.getString("name"));
+                aEmployee.setLastName(rs.getString("lastName"));
+                aEmployee.setPhone(rs.getString("phone"));
+                aEmployee.setUserName(rs.getString("username"));
+                aEmployee.setPassword(rs.getString("password"));
+                
+                //Agrego el empleado a la Lista
+                laListaDeRegistrosADevolver.add(aEmployee);
+            }
+            //Cierro conexiones
+            sentencia.close();
+            cn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            laListaDeRegistrosADevolver = null;
+        }
+        return laListaDeRegistrosADevolver;
+    }
      
 }
